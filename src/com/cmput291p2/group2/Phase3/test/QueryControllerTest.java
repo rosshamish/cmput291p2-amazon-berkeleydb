f3c9c9337e557e64505c74c66aa1d4060831c367
Ross Anderson <ross.anderson@ualberta.ca>
2015-11-18 11:18:10 -0700

    Add QueryController test skeletons

---
+++ src/com/cmput291p2/group2/Phase3/test/QueryControllerTest.java
@@ -0,0 +1,74 @@
+package com.cmput291p2.group2.Phase3.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QueryControllerTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void test1_pterm() {
+
+    }
+
+    @Test
+    public void test2_rterm() {
+
+    }
+
+    @Test
+    public void test3_query() {
+
+    }
+
+    @Test
+    public void test4_queryWithWildcard() {
+
+    }
+
+    @Test
+    public void test5_rtermAndQueryWithWildcard() {
+
+    }
+
+    @Test
+    public void test6_rscoreGTComparison() {
+
+    }
+
+    @Test
+    public void test7_queryAndRscoreLTComparison() {
+
+    }
+
+    @Test
+    public void test8_ppriceLTComparisonAndQuery() {
+
+    }
+    @Test
+    public void test8b_ppriceGTComparisonAndQuery() {
+
+    }
+
+    @Test
+    public void test9_queryAndRdateGTComparison() {
+
+    }
+    @Test
+    public void test9b_queryAndRdateLTComparison() {
+
+    }
+
+    @Test
+    public void test10_queryAndRdateGTComparisonAndPpriceGTComparisonAndPpriceLTComparison() {
+
+    }
+
+
+}
