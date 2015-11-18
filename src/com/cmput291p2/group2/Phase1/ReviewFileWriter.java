1a37c949299f5e13eb88ed7b59288708d8dfc288
Ross Anderson <ross.anderson@ualberta.ca>
2015-11-17 22:29:24 -0700

    Reformat ReviewFileWriter, rename some things

--- src/com/cmput291p2/group2/Phase1/ReviewFileWriter.java
+++ src/com/cmput291p2/group2/Phase1/ReviewFileWriter.java
@@ -3,16 +3,16 @@ package com.cmput291p2.group2.Phase1;
 import com.cmput291p2.group2.common.Debugging;
 import com.cmput291p2.group2.common.Review;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.io.FileWriter;
-import java.io.BufferedReader;
 
 /**
  * class ReviewFileWriter is responsible for reading reviews from stdin and
  * writing them to comma separated files in the format specified on eClass.
- *
+ * <p/>
  * The files it writers are used by
  * {@link com.cmput291p2.group2.Phase2.IndexBuilder}.
  */
@@ -23,118 +23,93 @@ public class ReviewFileWriter {
     private static final String rTermFile = "rterms.txt";
     private static final String scoreFile = "scores.txt";
     private static final String regexSplit = "[^0-9a-zA-Z_]";
-    private Integer count = 0;
 
     public void run(String filename) {
-        try(BufferedReader br = new BufferedReader(new FileReader(filename)))
-        {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
-            ArrayList<String> productDesc = new ArrayList();
-            count++;
-            while ((line = br.readLine()) != null)
-            {
-                if(line == "\n")
-                {
-                    Review product = new Review(productDesc);
-                    this.writeReview(product);
-                    this.writePTerm(product);
-                    this.writeRTerm(product);
-                    this.writeScore(product);
-                    productDesc = new ArrayList();
-                    count++;
-                }
-                else
-                {
-                    productDesc.add(line);
+            ArrayList<String> reviewDetails = new ArrayList<>();
+            while ((line = br.readLine()) != null) {
+                if (line.equals("\n")) {
+                    Review review = new Review(reviewDetails);
+                    this.writeReview(review);
+                    this.writePTerm(review);
+                    this.writeRTerm(review);
+                    this.writeScore(review);
+                    reviewDetails = new ArrayList<>();
+                } else {
+                    reviewDetails.add(line);
                 }
             }
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             if (Debugging.isEnabled()) {
-                System.err.printf("Filewrite IOException: %s", e.getMessage());
+                System.err.printf("Filewrite IOException: %s\n", e.getMessage());
             }
         }
     }
 
-    private void writeReview(Review product)
-    {
+    private void writeReview(Review review) {
         try {
             FileWriter fw = new FileWriter(reviewFile, true);
-            //As described by spec, should be: count, productid, title, price, userid, profilename, helpfulness, score
+            //As described by spec, should be: reviewid, productid, title, price, userid, profilename, helpfulness, score
             //time, summary and text
             //These fields should be escaped with "": title, profilename, summary, text
-            fw.write(count + "," + product.getReviewId() + ",\"" + product.getTitle() + "\"," +
-                    product.getPrice() + "," + product.getUserId() + ",\"" + product.getProfileName() +
-                    "\"," + product.getHelpfulness() + "," + product.getScore() + "," + product.getTime() + ",\"" +
-                    product.getSummary() + "\",\"" + product.getText() + "\"" + System.lineSeparator());
+            fw.write(String.valueOf(review.getReviewId()) + "," + review.getProductId() + ",\"" + review.getTitle() + "\"," +
+                    review.getPrice() + "," + review.getUserId() + ",\"" + review.getProfileName() +
+                    "\"," + review.getHelpfulness() + "," + review.getScore() + "," + review.getTime() + ",\"" +
+                    review.getSummary() + "\",\"" + review.getText() + "\"" + System.lineSeparator());
             fw.close();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             if (Debugging.isEnabled()) {
-                System.err.printf("Filewrite IOException: %s", e.getMessage());
+                System.err.printf("Filewrite IOException: %s\n", e.getMessage());
             }
         }
     }
 
-    private void writePTerm(Review product)
-    {
+    private void writePTerm(Review review) {
         try {
             FileWriter fw = new FileWriter(pTermFile, true);
-            for (String s : product.getTitle().split(regexSplit))
-            {
+            for (String s : review.getTitle().split(regexSplit)) {
                 if (s.length() >= 3) {
-                    fw.write(s.toLowerCase() + "," + count + System.lineSeparator());
+                    fw.write(s.toLowerCase() + "," + String.valueOf(review.getReviewId()) + System.lineSeparator());
                 }
             }
             fw.close();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             if (Debugging.isEnabled()) {
-                System.err.printf("Filewrite IOException: %s", e.getMessage());
+                System.err.printf("Filewrite IOException: %s\n", e.getMessage());
             }
         }
     }
 
-    private void writeRTerm(Review product)
-    {
+    private void writeRTerm(Review review) {
         try {
             FileWriter fw = new FileWriter(rTermFile, true);
-            for (String s : product.getSummary().split(regexSplit))
-            {
+            for (String s : review.getSummary().split(regexSplit)) {
                 if (s.length() >= 3) {
-                    fw.write(s.toLowerCase() + "," + count + System.lineSeparator());
+                    fw.write(s.toLowerCase() + "," + String.valueOf(review.getReviewId()) + System.lineSeparator());
                 }
             }
-            for (String s : product.getText().split(regexSplit))
-            {
+            for (String s : review.getText().split(regexSplit)) {
                 if (s.length() >= 3) {
-                    fw.write(s.toLowerCase() + "," + count + System.lineSeparator());
+                    fw.write(s.toLowerCase() + "," + String.valueOf(review.getReviewId()) + System.lineSeparator());
                 }
             }
             fw.close();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             if (Debugging.isEnabled()) {
-                System.err.printf("Filewrite IOException: %s", e.getMessage());
+                System.err.printf("Filewrite IOException: %s\n", e.getMessage());
             }
         }
     }
 
-    private void writeScore(Review product)
-    {
+    private void writeScore(Review review) {
         try {
             FileWriter fw = new FileWriter(scoreFile, true);
-            fw.write(product.getScore() + "," + count);
+            fw.write(review.getScore() + "," + String.valueOf(review.getReviewId()));
             fw.close();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             if (Debugging.isEnabled()) {
-                System.err.printf("Filewrite IOException: %s", e.getMessage());
+                System.err.printf("Filewrite IOException: %s\n", e.getMessage());
             }
         }
     }
