4051553fd3e88910c3d7ad0d5abb91b30f662072
Ross Anderson <ross.anderson@ualberta.ca>
2015-11-23 21:58:29 -0700

    add counter to see where they're at in the query

--- src/com/cmput291p2/group2/Phase3/test/QueryControllerTest.java
+++ src/com/cmput291p2/group2/Phase3/test/QueryControllerTest.java
@@ -197,7 +197,9 @@ public class QueryControllerTest {
 
         assertNotNull(reviews);
         assertTrue(reviews.size() > 0);
+        int counter = 0; //TODO remove
         for (Review review : reviews) {
+            counter++; //TODO remove
             assertTrue(review.getTitle().toLowerCase().contains(titleOrSummaryOrText) ||
                     review.getSummary().toLowerCase().contains(titleOrSummaryOrText) ||
                     review.getText().toLowerCase().contains(titleOrSummaryOrText));
@@ -205,6 +207,7 @@ public class QueryControllerTest {
                 System.out.printf("query: %s, review time: %s, rdate: %s\n",
                         query, review.getTime(), rdateLT);
             }
+            if (counter == reviews.size()) System.out.println("last, time: " + review.getTime()); //TODO remove
             // assertTrue(review.getTimeAsCalendar().before(rdateLTCal));
         }
     }
